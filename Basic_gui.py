5620f21e0482ea73ab478b3426f5dadbabcbaee7
AlexWoods1 <160354642+AlexWoods1@users.noreply.github.com>
2025-02-24 11:32:10 -0800

    Add files via upload

---
+++ Basic_gui.py
@@ -0,0 +1,44 @@
+import tkinter as tk
+import random
+import time
+import pandas as pd
+import datetime
+import os
+import regex
+topics = ['Success','Work','Education','Hobbies', 'Family', 'Travel','Food' ,'Pets','Movies','Music','Future Goals/Dreams']
+
+past = pd.DataFrame()
+
+def update_trial_info(event=None):
+    global past
+    truth_value = random.choice(["fact","lie"])
+    topic = random.choice(topics)
+    label.config(text=f"Please tell A {truth_value} about {topic}.")
+    current =pd.DataFrame(  {'time':time.time(), 'topic':topic, 'truth_value':truth_value}, index=[0])
+    past = pd.concat([past,current])
+
+def end_trial(event=None):
+    global past
+    past.to_csv(f"trial_{datetime.date.today()}.csv")
+    quit()
+
+# Create the main window
+root = tk.Tk()
+root.title("Random Number Generator")
+root.geometry("300x200")
+root.attributes('-fullscreen', True)
+
+# Create a label to display the number
+label = tk.Label(root, text="Press Space", font=("Arial", 24))
+label.pack(expand=True)
+
+
+# Pre Speaking
+root.bind("<KeyRelease-space>", update_trial_info)
+root.bind("<Return>",end_trial)
+# Post Speaking
+
+
+
+# Run the Tkinter event loop
+root.mainloop()
